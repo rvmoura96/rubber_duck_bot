8636791e0b4d84d8e5c7894951b3b64f1a486dec
Renan Moura <rvmoura.96@gmail.com>
2017-06-23 20:58:02 -0300

    Initial commit

---
+++ rubber_duck_bot.py
@@ -0,0 +1,30 @@
+from telegram.ext import Updater, CommandHandler
+from time import strftime
+
+
+up = Updater('BOT_ID')
+
+def Horas(bot, update):
+    msg = 'Olá {user_name} agora são: '
+    msg += strftime('%H:%M:%S')
+
+    bot.send_message(chat_id=update.message.chat_id,
+                     text=msg.format(
+                         user_name=update.message.from_user.first_name))
+                         
+def Duvida(bot, update):
+    msg = 'Me explica como funciona isso passo a passo'
+    bot.sendPhoto(chat_id=update.message.chat_id,
+                  photo=open('img/duck.png', 'rb'))
+    bot.send_message(chat_id=update.message.chat_id,
+        	        text=msg.format())
+
+def Comeca(bot, update):
+    bot.sendPhoto(chat_id=update.message.chat_id,
+                  photo=open('img/duck.png', 'rb'))
+
+up.dispatcher.add_handler(CommandHandler('duvidas', Duvida))                  
+up.dispatcher.add_handler(CommandHandler('horas', Horas))
+up.dispatcher.add_handler(CommandHandler('start', Comeca))
+up.dispatcher.add_handler(CommandHandler('help', Duvida))
+up.start_polling()
