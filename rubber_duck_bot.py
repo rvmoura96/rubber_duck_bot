17e8f89a92a75bdede7acc8e89f7315e3eaa9fdb
Renan Moura <rvmoura.96@gmail.com>
2017-06-30 20:17:21 -0300

    add func /ajuda

--- rubber_duck_bot.py
+++ rubber_duck_bot.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater, CommandHandler
 from time import strftime
 
 
-up = Updater('381143222:AAHLQXTyoSOx7YCpHI6WKgqkVr-9qoegvrA')
+up = Updater('')
 
 def Horas(bot, update):
     msg = 'Olá {user_name} agora são: '
@@ -22,6 +22,7 @@ def Duvida(bot, update):
 def Comeca(bot, update):
     bot.sendPhoto(chat_id=update.message.chat_id,
                   photo=open('img/duck.png', 'rb'))
+                  
 
 up.dispatcher.add_handler(CommandHandler('duvidas', Duvida))                  
 up.dispatcher.add_handler(CommandHandler('horas', Horas))
